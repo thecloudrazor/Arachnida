c9ee6c93776c014eb47fa4c407bc264f910ddbc5
whymami <mmg165316@gmail.com>
2024-12-18 23:30:18 +0300

    parser düzeltildi

--- spider.py
+++ spider.py
@@ -1,46 +1,71 @@
+import argparse
 import requests
 from bs4 import BeautifulSoup
-import sys
+import pathlib
 
-option_r = False
-option_l = 5
-option_p = "./data/"
-url = ""
+EXTENSIONS = [".jpg", "jpeg", ".png", ".gif", ".bmp"]
+DEFAULT_DEPTH = 5
+DEFAULT_PATH = "./data/"
 
-def ft_err(msg):
-    print(msg)
-    exit(1)
-
-def check_arg(ac,av):
-    global option_l, option_p, option_r
-
-    i = 1
-    while i < ac:
-        arg = av[i]
-        if arg == "-r":
-            option_r = True
-        elif arg == "-l":
-            if i + 1 < ac and av[i + 1].isdigit():
-                option_l = int(av[i + 1])
-                i += 1
-            else:
-                ft_err("Option -l requires a number after it.")
-        elif arg == "-p":
-            if i + 1 < ac:
-                option_p = av[i + 1]
-                i += 1
+def parse_args():
+    parser = argparse.ArgumentParser(description="SpiderBot")
+    parser.add_argument('-r', '--recursive', action='store_true', help='Download images recursively (to depth level 5 by default)')
+    parser.add_argument('-l', '--level', dest='depth', type=int, help='Depth level of recursive image search')
+    parser.add_argument('-p', '--path', type=pathlib.Path, help='Path to save downloaded images')
+    parser.add_argument('URL', help='URL to download images from')
+    
+    args = parser.parse_args()
+    if args.depth is None:
+        if args.recursive:
+            args.depth = DEFAULT_DEPTH
         else:
-            ft_err(f"Invalid option {arg}")
-        i += 1
-    print("option_l:", option_l)
-    print("option_p:", option_p)
-    print("option_r:", option_r)
+            args.depth = 1
+    elif args.depth and (args.recursive is False):
+        parser.error("argument -l/--level: expected -r/--recursive argument.")
+    if args.path is None:
+        args.path = pathlib.Path(DEFAULT_PATH)
+    args.current_depth = 0
+    return args
 
+def print_arg(args):
+    print("Arguments:")
+    print(f"Recursive: {args.recursive}")
+    print(f"Depth: {args.depth}")
+    print(f"Path: {args.path}")
+    print(f"URL: {args.URL}")
+
+def check_url(args):
+    url = args.URL
+    if not url.startswith('http://') and not url.startswith('https://'):
+        url = 'http://' + url
+    try:
+        response = requests.get(url)
+        if (response.status_code != 200):
+            print("error: incorrect url")
+            exit(1)
+    except requests.exceptions.RequestException as e:
+        print(f"error: {e}")
+        exit(1)
+
+def check_path(args):
+    if args.path:
+        path = pathlib.Path(args.path)
+
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
+        elif not path.is_dir():
+            print(f"Error: The specified path '{args.path}' is not a directory.")
+            exit(1)
+        else:
+            print(f"Path '{args.path}' is a valid directory.")
 
 def main():
-    if (len(sys.argv) < 2):
-        ft_err("Usage: python3 spider.py [-r] [-l] [depth level] [-p] [path] [URL]")
-    check_arg(len(sys.argv) - 1 , sys.argv)
+    args = parse_args()
+    print_arg(args)
+    check_path(args)
+    check_url(args)
 
-if (__name__ == "__main__"):
+    print("Scraping işlemleri başlatılıyor...")
+
+if __name__ == "__main__":
     main()
