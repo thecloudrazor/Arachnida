c4c23cfa78db6b15afde7624a67812b043679293
whymami <mmg165316@gmail.com>
2024-12-19 13:07:48 +0300

    create win

--- bonus/gui.py
+++ bonus/gui.py
@@ -1,3 +1,17 @@
-import tkinter as tk
-from tkinter import filedialog, messagebox
-from tkinter import simpledialog
+from tkinter import mainloop,Tk,Label,Button,filedialog
+from pathlib import Path
+
+
+def selam():
+    select = filedialog.askopenfilenames()
+    print(select)
+
+
+win = Tk()
+win.title("Scorpion") #pencerenin başlığını belirliyor
+win.geometry("500x500") #pencerenin boyunu ayarlyıor
+win.resizable(False, False) #pencereyi yeniden boyutlandırmayı iptal ediyor ilk parametre en diğeri boy
+win.wm_attributes("-topmost", 1) #ilk iki parametre saydamlık veriyor diğer iki paramtre en önde kalmasını sağlıyor
+Label(text="Selam", fg="green",cursor="spider").pack() #label ile bir label oluşturup pack() ile windowa paketliyorz
+Button(text="Select Image", command=selam).pack()
+mainloop()
